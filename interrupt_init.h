776524ca2780e9045287ace7af86d86b3d367b20
jacklynf <finnemey@usc.edu>
2024-04-10 13:58:18 -0700

    updated timer to 1 second

--- interrupt_init.h
+++ interrupt_init.h
@@ -1,4 +1,4 @@
-#define WAIT_TIME 0.2 // 200 ms wait time -- adjust this value for shorter/longer timer
+#define WAIT_TIME 1 // 1 second wait time -- adjust this value for shorter/longer timer
 #define CLOCK 9830400
 #define PRESCALAR 1024
 #define TICK_TIME 1/(CLOCK/PRESCALAR)
