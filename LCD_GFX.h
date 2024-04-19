40b8ccd4ecbba132502220711887fc07a038db28
dkress <dkress@usc.edu>
2024-04-19 14:47:17 -0700

    LCD Commit #7.3
    Screwed up GitHub Restoring now

--- LCD_GFX.h
+++ LCD_GFX.h
@@ -59,15 +59,7 @@ void setTextSize(uint8_t s);
     @param   bg  16-bit 5-6-5 Color to draw background/fill with
   */
   /**********************************************************************/
-<<<<<<< HEAD
-<<<<<<< HEAD
-  void setTextandBGColor(uint16_t c, uint16_t bg) {
-=======
-  void setTextColor(uint16_t c, uint16_t bg) {
->>>>>>> parent of 8617010 (LCD Update #7.1)
-=======
   void setTextanBGColor(uint16_t c, uint16_t bg) {
->>>>>>> parent of e3e0b79 (LCD Update #7.3)
     textcolor = c;
     textbgcolor = bg;
   }
@@ -82,6 +74,6 @@ void setTextSize(uint8_t s);
   void setTextWrap(bool w) { wrap = w; }
 
   void printString(const char* string);
-  
+
 #endif  //LCD_GFX
 
