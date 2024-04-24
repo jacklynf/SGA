fc16d3a83395dbc7c326ad79592bf4f12d271d94
dkress <dkress@usc.edu>
2024-04-24 09:43:48 -0700

    More encoder changes

--- encoder.c
+++ encoder.c
@@ -2,6 +2,10 @@
 #include <avr/interrupt.h>
 #include "encoder.h"
 
+volatile uint8_t encoder_old_state, encoder_new_state; // state machine
+volatile uint8_t encoder_changed; // flag
+volatile uint8_t encoder_input, encoder_A, encoder_B; //rotary encoder inputs variables
+
 uint8_t init_encoder(){
     DDRC   &= ~(ENCODERA|ENCODERB); //Set EncoderA and EncoderB pins as inputs (constants defined in encoder.h)
     PORTC  |=  (ENCODERA|ENCODERB); //Enable Pull-Up resistor on EncoderA and EncoderB
--- encoder.h
+++ encoder.h
@@ -1,13 +1,13 @@
-#define ENCODERA    (1 << PC3)
-#define ENCODERB    (1 << PC2)
+#define ENCODERA    (1 << PC2)
+#define ENCODERB    (1 << PC3)
 
 #define WATER_IDX 0
 #define LIGHT_IDX 1
 
 // Volatile encoder variables for interrupt
-volatile uint8_t encoder_old_state, encoder_new_state; // state machine
-volatile uint8_t encoder_changed; // flag
-volatile uint8_t encoder_input, encoder_A, encoder_B; //rotary encoder inputs variables
+volatile extern uint8_t encoder_old_state, encoder_new_state; // state machine
+volatile extern uint8_t encoder_changed; // flag
+volatile extern uint8_t encoder_input, encoder_A, encoder_B; //rotary encoder inputs variables
 
 // Set by user via rotary encoder
 enum PLANT_NEEDS{
